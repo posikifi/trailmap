566cee5725c04b18aa3909f2b2ded9036d133e19
nopsa_nopsa <nopsa@me.com>
2013-10-21 16:07:48 +0300

    vienti tietokantaan lisatty, ei viela testattu, kun tauluja ei ole.

--- src/gpx2db.php
+++ src/gpx2db.php
@@ -1,6 +1,8 @@
 <?php
-function vieData($gpxtiedosto)
+function vieData($gpxtiedosto, $trackID)
 {
+	$con = pg_connect("host=localhost dbname=trailmap_gis user=KAYTTAJA password=SALASANA") or die ("Could not connect to server\n");
+	//yhteys aina luodaan uudestaan, vai tuleeko yhteys parametrina?
 	$tiedosto = simplexml_load_file($gpxtiedosto);
 	foreach ($tiedosto->trk as $trk)
 	{
@@ -13,15 +15,20 @@ function vieData($gpxtiedosto)
 				$lon = $tiedot["lon"];
 				$time = $trkpt->time;
 				$ele = $trkpt->ele;
-				//vienti tietokantaan puuttuu viela
+				//vienti tietokantaan
+				$query = "INSERT INTO trkpt (time,z,track_id,geom) VALUES($1, $2, $3, ST_Point($4,$5))";
+				pg_prepare($con, "valmistelu", $query) or die ("Cannot prepare statement\n");
+				pg_execute($con, "valmistelu", array($time, $ele, $trackID,$lon,$lat)) or die ("Cannot execute statement\n");
+				//piste varmaan viety
 			}
 		}
 	}
+	pg_close($con);
 }
 
 
 echo "\n";	
-vieData('Move_testi.gpx');
+vieData('Move_testi.gpx', 12);
 echo "\n\n";
 
 
