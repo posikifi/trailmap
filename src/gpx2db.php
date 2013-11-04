e3b094fdb2c5b311ced381667698b12c0073a34c
n0ps <nopsa@me.com>
2013-11-04 14:54:57 +0200

    kaikkea säädetty

--- src/gpx2db.php
+++ src/gpx2db.php
@@ -1,10 +1,13 @@
 <?php
-function vieData($gpxtiedosto, $trackID, $user)
+function vieData($gpxtiedosto, $user)
 {
-	$con = pg_connect("host=localhost dbname=trailmap_gis user=KAYTTAJA password=SALASANA") or die ("Could not connect to server\n");
+	$con = pg_connect("host=localhost dbname=trailmap_gis user=trailmap password=luoto") or die ("Could not connect to server\n");
+	$query = "INSERT INTO trkpt (time,z,track_id,geom) VALUES($1, $2, $3, ST_SetSRID(ST_Point($4,$5),4326))";
+	pg_prepare($con, "valmistelu", $query) or die ("Cannot prepare statement\n");
+	pg_query('SELECT nextval('tracks.id') FROM raw.tracks');
+	$trackID = pg_fetch_row()[0];
 	//yhteys aina luodaan uudestaan, vai tuleeko yhteys parametrina?
 	$tiedosto = simplexml_load_file($gpxtiedosto);
-	$pisteet = array();
 	foreach ($tiedosto->trk as $trk)
 	{
 		foreach ($trk->trkseg as $trkseg)
@@ -17,28 +20,12 @@ function vieData($gpxtiedosto, $trackID, $user)
 				$time = $trkpt->time;
 				$ele = $trkpt->ele;
 				//vienti tietokantaan
-				$query = "INSERT INTO trkpt (time,z,track_id,geom) VALUES($1, $2, $3, ST_Point($4,$5))";
-				pg_prepare($con, "valmistelu", $query) or die ("Cannot prepare statement\n");
-				pg_execute($con, "valmistelu", array($time, $ele, $trackID,$lon,$lat)) or die ("Cannot execute statement\n");
-				//piste varmaan viety
-				array_push($pisteet,sprintf("%s %s",$lat,$lon));
-				
+				pg_execute($con, "valmistelu", array($time, $ele, $trackID,$lon,$lat)) or die ("Cannot execute statement\n");				
 			}
 		}
 	}
-	//muodostetaan linestring
-	$line = "LINESTRING (".implode(",",$pisteet).")";
-	$query = "INSERT INTO track (user_id,geom) VALUES($1, ST_GeomFromText($2,4326))";
-	pg_prepare($con, "valmistelu", $query) or die ("Cannot prepare statement\n");
-	pg_execute($con, "valmistelu", array($user, $line)) or die ("Cannot execute statement\n");
-	
-	pg_close($con);
 }
 
-
-//echo "\n";	
-//vieData('Move_testi.gpx', 12, 1234);
-//echo "\n\n";
 
 
 ?>
