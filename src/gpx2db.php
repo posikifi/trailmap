8ee46a1d87bd417a158a087247aff99e41ffd197
n0ps <nopsa@me.com>
2013-11-04 08:26:58 +0200

    nyt taitaa tallentaa trackinkin

--- src/gpx2db.php
+++ src/gpx2db.php
@@ -1,9 +1,10 @@
 <?php
-function vieData($gpxtiedosto, $trackID)
+function vieData($gpxtiedosto, $trackID, $user)
 {
 	$con = pg_connect("host=localhost dbname=trailmap_gis user=KAYTTAJA password=SALASANA") or die ("Could not connect to server\n");
 	//yhteys aina luodaan uudestaan, vai tuleeko yhteys parametrina?
 	$tiedosto = simplexml_load_file($gpxtiedosto);
+	$pisteet = array();
 	foreach ($tiedosto->trk as $trk)
 	{
 		foreach ($trk->trkseg as $trkseg)
@@ -20,16 +21,24 @@ function vieData($gpxtiedosto, $trackID)
 				pg_prepare($con, "valmistelu", $query) or die ("Cannot prepare statement\n");
 				pg_execute($con, "valmistelu", array($time, $ele, $trackID,$lon,$lat)) or die ("Cannot execute statement\n");
 				//piste varmaan viety
+				array_push($pisteet,sprintf("%s %s",$lat,$lon));
+				
 			}
 		}
 	}
+	//muodostetaan linestring
+	$line = "LINESTRING (".implode(",",$pisteet).")";
+	$query = "INSERT INTO track (user,,geom) VALUES($1, ST_GeomFromText($2,4326))";
+	pg_prepare($con, "valmistelu", $query) or die ("Cannot prepare statement\n");
+	pg_execute($con, "valmistelu", array($user, $line)) or die ("Cannot execute statement\n");
+	
 	pg_close($con);
 }
 
 
-echo "\n";	
-vieData('Move_testi.gpx', 12);
-echo "\n\n";
+//echo "\n";	
+//vieData('Move_testi.gpx', 12, 1234);
+//echo "\n\n";
 
 
 ?>
