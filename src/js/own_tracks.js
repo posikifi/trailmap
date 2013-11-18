2c57fb95f49ab05ff520780b91ce6697492093a3
GIS AD <gisad@gisad.kyla.fi>
2013-11-18 21:37:36 +0200

    Linkkisaatoa...

--- src/js/own_tracks.js
+++ src/js/own_tracks.js
@@ -1,1 +1,23 @@
-/var/www/trailmap/own_tracks.js
+jQuery(function() {
+    var $ = jQuery;
+    var lat,lon,zoom,map;
+    lon = 26;
+    lat = 62;
+    zoom = 6;
+    var wgs84 = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var spher   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+
+        map = new OpenLayers.Map( 'map',{           
+            numZoomLevels: 20,
+            projection: new OpenLayers.Projection("EPSG:900913"),
+            displayProjection: new OpenLayers.Projection("EPSG: 4326")
+        });
+        var osmlayer = new OpenLayers.Layer.OSM( "OSM");
+        map.addLayer(osmlayer);
+        var tracklayer = new OpenLayers.Layer.WMS( "Tracks",
+                    "./mapproxy.php", {layers: 'trailmap:tracks',transparent:true},{isBaseLayer:false} );
+        map.addLayer(tracklayer);
+
+        map.setCenter(new OpenLayers.LonLat(lon, lat).transform(wgs84,spher), zoom);
+        map.addControl( new OpenLayers.Control.LayerSwitcher() );
+});
